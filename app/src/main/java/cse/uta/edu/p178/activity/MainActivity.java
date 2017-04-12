955ffb4d437d181f5e640821586420a61afe6678
Luke <zhongwei.li@mavs.uta.edu>
2017-04-11 21:19:37 -0500

    Update: 1) UI fixed 2) Main function: read left/right foot pressure

--- app/src/main/java/cse/uta/edu/p178/activity/MainActivity.java
+++ app/src/main/java/cse/uta/edu/p178/activity/MainActivity.java
@@ -1,12 +1,16 @@
 package cse.uta.edu.p178.activity;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -15,7 +19,10 @@ import android.widget.TextView;
 import java.lang.ref.WeakReference;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import cse.uta.edu.p178.DeviceData;
 import cse.uta.edu.p178.R;
 import cse.uta.edu.p178.Utils;
 import cse.uta.edu.p178.bluetooth.DeviceConnector;
@@ -39,6 +46,8 @@ public class MainActivity extends BaseActivity {
 
     private StringBuilder logHtml;
     private TextView logTextView;
+    private TextView leftReadTextView;
+    private TextView rightReadTextView;
     private EditText commandEditText;
 
     // Настройки приложения
@@ -46,12 +55,39 @@ public class MainActivity extends BaseActivity {
     private boolean show_timings, show_direction;
     private String command_ending;
     private String deviceName;
+    private String[] lastRead;
 
     //GUI
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (mHandler == null) mHandler = new BluetoothResponseHandler(this);
+        else mHandler.setTarget(this);
+
         setContentView(R.layout.activity_main);
+
+        if (isConnected() && (savedInstanceState != null)) {
+            setDeviceName(savedInstanceState.getString(DEVICE_NAME));
+        } else getActionBar().setSubtitle(MSG_NOT_CONNECTED);
+
+        this.logHtml = new StringBuilder();
+        if (savedInstanceState != null) this.logHtml.append(savedInstanceState.getString(LOG));
+
+//        this.logTextView = (TextView) findViewById(R.id.log_textview);
+//        this.logTextView.setMovementMethod(new ScrollingMovementMethod());
+        this.leftReadTextView = (TextView) findViewById(R.id.tv_L);
+        this.rightReadTextView = (TextView) findViewById(R.id.tv_R);
+
+////        this.logTextView.setText(Html.fromHtml(logHtml.toString()));
+//        if(logHtml.toString().contains(","))
+//            lastRead = logHtml.toString().substring(logHtml.toString().lastIndexOf("\n")).split(",");
+//
+//        if(lastRead != null && lastRead.length > 1) {
+//            leftReadTextView.setText(lastRead[0]);
+//            rightReadTextView.setText(lastRead[1]);
+//        }
+
     }
 
     @Override
@@ -87,6 +123,40 @@ public class MainActivity extends BaseActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CONNECT_DEVICE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK) {
+                    String address = data.getStringExtra(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+                    BluetoothDevice device = btAdapter.getRemoteDevice(address);
+                    if (super.isAdapterReady() && (connector == null)) setupConnector(device);
+                }
+                break;
+            case REQUEST_ENABLE_BT:
+                // When the request to enable Bluetooth returns
+                super.pendingRequestEnableBt = false;
+                if (resultCode != Activity.RESULT_OK) {
+                    Utils.log("BT not enabled");
+                }
+                break;
+        }
+    }
+    private void setupConnector(BluetoothDevice connectedDevice) {
+        stopConnection();
+        try {
+            String emptyName = getString(R.string.empty_device_name);
+            DeviceData data = new DeviceData(connectedDevice, emptyName);
+            connector = new DeviceConnector(data, mHandler);
+            connector.connect();
+        } catch (IllegalArgumentException e) {
+            Utils.log("setupConnector failed: " + e.getMessage());
+        }
+    }
+
 
     private void startDeviceListActivity() {
         stopConnection();
@@ -126,12 +196,26 @@ public class MainActivity extends BaseActivity {
                 .append("<br>");
 
         logHtml.append(msg);
-        logTextView.append(Html.fromHtml(msg.toString()));
 
-        final int scrollAmount = logTextView.getLayout().getLineTop(logTextView.getLineCount()) - logTextView.getHeight();
-        if (scrollAmount > 0)
-            logTextView.scrollTo(0, scrollAmount);
-        else logTextView.scrollTo(0, 0);
+        //add to show left/right read
+        String[] lastRead = null;
+        if(!message.isEmpty() && message.contains(","))
+            lastRead = message.split(",");
+
+        if(lastRead != null && lastRead.length == 2) {
+//            Log.d(LOG, "FOUND READ left = " + lastRead[0]);
+//            Log.d(LOG, "FOUND READ right = " + lastRead[1]);
+            leftReadTextView.setText(lastRead[0]);
+            rightReadTextView.setText(lastRead[1]);
+        }
+
+
+//        logTextView.append(Html.fromHtml(msg.toString()));
+
+//        final int scrollAmount = logTextView.getLayout().getLineTop(logTextView.getLineCount()) - logTextView.getHeight();
+//        if (scrollAmount > 0)
+//            logTextView.scrollTo(0, scrollAmount);
+//        else logTextView.scrollTo(0, 0);
 
         if (clean) commandEditText.setText("");
     }
